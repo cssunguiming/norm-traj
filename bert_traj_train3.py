61a8622fd85608581588c324d66d417d84b150ab
cssunguiming <56591943+cssunguiming@users.noreply.github.com>
2020-06-24 15:19:02 +0800

    3

--- bert_traj_train3.py
+++ bert_traj_train3.py
@@ -197,8 +197,8 @@ def main(Epoch=200, Bert_Pretrain=False, Batch_size=10, Pretrained=False, log='p
 
     head_n = 10
     d_model = 500
-    N_layers = 10
-    dropout = 0.4
+    N_layers = 3
+    dropout = 0.5
 
     # head_n = 10
     # d_model = 400
